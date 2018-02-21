92a40b271352b18528b9ed672e9225ef6fd08cc1
davis009 <davis6247@gmail.com>
2018-02-21 19:49:48 +0300

    --

--- src/index.js
+++ src/index.js
@@ -1,26 +1,54 @@
- module.exports =function getZerosCount(number, base) { 
-  var prime = findPrime(base); //we got zero at the end of number  by product of primes
-  var zeros = 0;
-
-  for(var i = prime; i  < number; i *= prime){
-    zeros += Math.floor(number / i); 
-  }
+ module.exports =  function getZerosCount(number, base){
   
+  var  divisionCount,zeros,num,possibleZeros = number;;  
+  
+  for (var i = 2; i <= base;i++)
+  {
+    if (base % i == 0)
+    {   
+      divisionCount = 0;
+      while (base % i == 0)
+      {
+        divisionCount++;
+        base = Math.floor(base / i);  
+      }
+      var possibleZeros = 0;
+      num = number;
+
+      while (Math.floor(k / i) > 0)
+      {
+        possibleZeros += Math.floor(k / i);
+        num = Math.floor(num / i);
+      }
+      zeros = Math.min (zeros , Math.floor(zeros / divisionCount));
+    }
+}        
   return zeros;
 }
 
-function findPrime(base){
-  var prime = base;
+//fisrt attempt
+// module.exports = function getZerosCount(number, base) { 
+//   var prime = findPrime(base); //we got zero at the end of number  by product of primes
+//   var zeros = 0;
 
-for(var i = 2; i <= prime / 2; i++){
-  if(prime % i == 0){
-      if(Math.floor(prime / i) == i) break;
-      prime = Math.floor(prime / i);
-      //if(prime < leastPrime) leastPrime = prime;
-      i = 1;
-  }
-}
+//   for(var i = prime; i  < number; i *= prime){
+//     zeros += Math.floor(number / i); 
+//   }
+  
+//   return zeros;
+// }
 
-  return prime;
-}
+// function findPrime(base){
+//   var prime = base;
 
+// for(var i = 2; i <= prime / 2; i++){
+//   if(prime % i == 0){
+//       //if(Math.floor(prime / i) == i) break;
+//       prime = Math.floor(prime / i);
+//       //if(prime < leastPrime) leastPrime = prime;
+//       i = 1;
+//   }
+// }
+
+//   return prime;
+// }
